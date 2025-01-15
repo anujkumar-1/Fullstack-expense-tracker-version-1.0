2fe7bdadec4f372f506dc97d524af9dc5ece30f9
anujkumar-1 <110293115+anujkumar-1@users.noreply.github.com>
2025-01-15 13:15:34 +0530

    Update User.js
    updated on 15 jan 2025

--- controllers/User.js
+++ controllers/User.js
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export function generateAccessTokens(id, name, ispremiumuser, totalCost){
-    const token = jwt.sign({userId: id, name:name, ispremiumuser, totalCost}, "AK47")
+    const token = jwt.sign({userId: id, name:name, ispremiumuser, totalCost}, process.env.JWT_TOKEN_SECRET)
     return token
 }
 
