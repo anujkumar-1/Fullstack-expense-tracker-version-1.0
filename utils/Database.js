7f0c748bb6c880f438aa441fe653d168d873fc3f
anujkumar-1 <110293115+anujkumar-1@users.noreply.github.com>
2025-01-15 14:12:23 +0530

    Update Database.js
    updated at 15 jan 2025

--- utils/Database.js
+++ utils/Database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize( "expdb", "root", "Anujkumar@1", {
+const sequelize = new Sequelize( process.env.DB_NAME, process.env.DB_USER_NAME, process.env.DB_USER_PASSWORD, {
     host: "localhost",
     dialect: "mysql"
 
