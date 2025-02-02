24c605e8068415269225df545377406c5ad05ea0
anujkumar-1 <110293115+anujkumar-1@users.noreply.github.com>
2025-02-02 09:35:14 +0530

    Update Database.js

--- utils/Database.js
+++ utils/Database.js
@@ -1,5 +1,5 @@
 import { Sequelize } from 'sequelize';
-
+dotenv.config()
 const sequelize = new Sequelize( process.env.DB_NAME, process.env.DB_USER_NAME, process.env.DB_USER_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: "mysql"
